c900d2cc2fa31d34492fb61e220757d9cf99fd59
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2024-04-01 14:41:44 +0200

    Add uvm tests, update sv-tests and update_tests script

--- update_tests.sh
+++ update_tests.sh
@@ -4,6 +4,7 @@ PATTERN=$1
 
 # First create the directory structure to prevent copy errors
 TESTS=( $(find sv-tests -type d) )
+TESTS+=( $(find uvm -type d) )
 
 for dir in ${TESTS[@]}; do
     mkdir -p test/corpus/$dir
@@ -11,7 +12,9 @@ done
 
 # Retrieve all the test files
 FILES_SV=( $(find sv-tests -type f -name "*.sv") )
-FILES_SVH=( $(find sv-tests -type f -name "*.svh") )
+FILES_SV+=( $(find sv-tests -type f -name "*.svh") )
+FILES_SV+=( $(find uvm -type f -name "*.sv") )
+FILES_SV+=( $(find uvm -type f -name "*.svh") )
 
 # Declare variables
 DIR_FILENAME=
@@ -21,11 +24,16 @@ DEST_FILE=
 LAST_FILE_LINE=
 
 # Expected failure tests
-EXPECTED_FAIL_FILELIST=(test/corpus/sv-tests/chapter-5/5.6--wrong-identifiers.sv
-                        test/corpus/sv-tests/chapter-5/5.7.1--integers-signed-illegal.sv
-                        test/corpus/sv-tests/chapter-5/5.7.1--integers-unsized-illegal.sv
-                        test/corpus/sv-tests/chapter-5/5.7.2-real-constants-illegal.sv
-                        test/corpus/sv-tests/chapter-5/5.10-structure-arrays-illegal.sv)
+EXPECTED_FAIL_FILELIST=(sv-tests/chapter-5/5.6--wrong-identifiers.sv
+                        sv-tests/chapter-5/5.7.1--integers-signed-illegal.sv
+                        sv-tests/chapter-5/5.7.1--integers-unsized-illegal.sv
+                        sv-tests/chapter-5/5.7.2-real-constants-illegal.sv
+                        sv-tests/chapter-5/5.10-structure-arrays-illegal.sv)
+
+# Exclude tests
+EXCLUDED_FILELIST=(sv-tests/chapter-5/5.6.4--compiler-directives-preprocessor-macro_0.sv # No intention of supporting preprocessing
+                   sv-tests/chapter-5/5.10-structure-arrays-illegal.sv) # No intention of detecting the /* C-like assignment is illegal */ for struct initialization
+
 
 # Filter tests, if there was an argument provided
 if [[ -n "$PATTERN" ]]; then
@@ -34,37 +42,43 @@ if [[ -n "$PATTERN" ]]; then
 fi
 
 # Create tests
-for file in ${FILES_SV[@]}; do
+for file in "${FILES_SV[@]}"; do
     DIR_FILENAME=$(dirname $file)
     BASE_FILENAME=$(basename $file)
     FILENAME_NO_EXT=${BASE_FILENAME%.sv}
     DEST_FILE=test/corpus/$DIR_FILENAME/${FILENAME_NO_EXT}.txt
 
-    echo "============================================" > $DEST_FILE
-    echo "$DIR_FILENAME/$FILENAME_NO_EXT" >> $DEST_FILE
-    [[ "${EXPECTED_FAIL_FILELIST[@]}" =~ "$file" ]] && echo ":error" >> $DEST_FILE
-    echo "============================================" >> $DEST_FILE
-    echo "" >> $DEST_FILE
-
-    cat $file >> $DEST_FILE
-    echo "" >> $DEST_FILE
-    echo "----" >> $DEST_FILE
-    echo "" >> $DEST_FILE
-
-    tree-sitter parse $file >> $DEST_FILE
-    sed -i -E 's/ \[[0-9]+, [0-9]+\] - \[[0-9]+, [0-9]+\]//g' $DEST_FILE # Remove brackets with node positions
-
-    # Check if file has errors and reformat expected file in that case (remove last line)
-    LAST_FILE_LINE=$(tail -1 $DEST_FILE | grep -E 'ERROR|MISSING')
-    if [[ $LAST_FILE_LINE ]]; then
-        # Remove last file line before formatting
-        sed -i '$ d' $DEST_FILE
-        # Set new last file line without statistics
-        LAST_FILE_LINE=$(echo $LAST_FILE_LINE | cut -d " " -f 6-)
+    if [[ "${EXCLUDED_FILELIST[@]}" =~ "$file" ]]; then
+        echo "Excluding $file ..."
+        cat $file > $DEST_FILE
+    else
+        echo "============================================" > $DEST_FILE
+        echo "$DIR_FILENAME/$FILENAME_NO_EXT" >> $DEST_FILE
+        [[ "${EXPECTED_FAIL_FILELIST[@]}" =~ "$file" ]] && echo ":error" >> $DEST_FILE
+        echo "============================================" >> $DEST_FILE
         echo "" >> $DEST_FILE
-        echo "$LAST_FILE_LINE" >> $DEST_FILE
+
+        cat $file >> $DEST_FILE
+        echo "" >> $DEST_FILE
+        echo "----" >> $DEST_FILE
+        echo "" >> $DEST_FILE
+
+        tree-sitter parse $file >> $DEST_FILE
+        sed -i -E 's/ \[[0-9]+, [0-9]+\] - \[[0-9]+, [0-9]+\]//g' $DEST_FILE # Remove brackets with node positions
+
+        # Check if file has errors and reformat expected file in that case (remove last line)
+        LAST_FILE_LINE=$(tail -1 $DEST_FILE | grep -E 'ERROR|MISSING')
+        if [[ $LAST_FILE_LINE ]]; then
+            # Remove last file line before formatting
+            sed -i '$ d' $DEST_FILE
+            # Set new last file line without statistics
+            LAST_FILE_LINE=$(echo $LAST_FILE_LINE | cut -d " " -f 6-)
+            echo "" >> $DEST_FILE
+            echo "$LAST_FILE_LINE" >> $DEST_FILE
+        fi
+
+        echo "Updated $DEST_FILE"
     fi
 
-    echo "Updated $DEST_FILE"
 done
 
