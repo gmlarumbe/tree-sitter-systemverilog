75ce0bb0e5ba448e43bc05db93bbeb2fa1543423
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2024-04-02 17:22:45 +0200

    Update tests

--- test/larumbe/test.sv
+++ test/larumbe/test.sv
@@ -2,9 +2,9 @@ module top();
 
 initial begin
     bit [95:0] d = {<<8{a, b, c}};
-    bit [95:0] d = {<<{a, b, c}};
+    // bit [95:0] d = {<<{a, b, c}};
     // bit [95:0] d = {>>{a, b}};
-    // d = {>> 8 {a, b}};
+    d = {>> {a, b}};
     // d = { << 8{a, b, c}};
 end
 
