0dacb911daa9614a7c7e79a594d4cb9f478e6554
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2024-09-28 20:24:15 +0200

    Update generated files

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_verilog_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-verilog"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_verilog "github.com/tree-sitter/tree-sitter-verilog/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
